645fa627c432b76d8139bdab49b8e7acaf6b19bc
tirlochan016 <trilochan@crashnova.com>
2024-10-31 00:16:01 +0530

    dev server working correctly

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world 123"))
+	})
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Hello world")
 }
