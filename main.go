bd4780d0a520bd8e84aef1d2549168d036db8fd8
tirlochan016 <trilochan@crashnova.com>
2024-10-31 00:44:51 +0530

    created DB connection with GORM

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := ""
+
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("error establishing DB connection")
+	}
+
+	fmt.Println("DB connected successfully")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world 123"))
 	})
 
-	err := http.ListenAndServe(":3000", nil)
+	err = http.ListenAndServe(":3000", nil)
 
 	if err != nil {
 		panic(err)
